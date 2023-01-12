519213a879771eb744a8e773972845c1373ec426
ThinhBeo <59316091+Thinhle2701@users.noreply.github.com>
2023-01-12 09:02:06 +0700

    commit QRcode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
-import { useRef } from 'react';
+import { useRef ,useState} from 'react';
 import './App.css';
 import Webcam from "react-webcam"
+import QrReader from 'modern-react-qr-reader'
 function App() {
-  const webref = useRef(null)
+
+  const [scanResultWebCam, setScanResultWebCam] =  useState('');
+  const webref = useRef(null);
+  const handleErrorWebCam = (error) => {
+    console.log(error);
+  }
+  const handleErrorFile = (error) => {
+    console.log(error);
+  }
+  const handleScanWebCam = (result) => {
+    if (result){
+        setScanResultWebCam(result);
+    }
+  }
   return (
-    <div className="App">
-      React Webcam
-      <Webcam ref={webref}/>
-    </div>
+    <div>
+    <QrReader
+      delay={300}
+      onError={handleErrorFile}
+      onScan={handleScanWebCam}
+      constraints={ {facingMode: 'environment'} }
+      style={{ width: '70%' }}
+    />
+    <p>{scanResultWebCam}</p>
+  </div>
   );
 }
 
