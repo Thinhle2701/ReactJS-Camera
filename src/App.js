d29faf59e94f2a5400444b13063cf036d170a3f8
ThinhBeo <59316091+Thinhle2701@users.noreply.github.com>
2023-01-12 09:27:36 +0700

    test barcode

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { useRef ,useState} from 'react';
 import './App.css';
 import Webcam from "react-webcam"
 import QrReader from 'modern-react-qr-reader'
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 function App() {
-
+  const [data, setData] = useState("Not Found");
   const [scanResultWebCam, setScanResultWebCam] =  useState('');
   const webref = useRef(null);
   const handleErrorWebCam = (error) => {
@@ -20,14 +21,23 @@ function App() {
   }
   return (
     <div>
-    <QrReader
+    {/* <QrReader
       delay={300}
       onError={handleErrorFile}
       onScan={handleScanWebCam}
       constraints={ {facingMode: 'environment'} }
       style={{ width: '70%' }}
     />
-    <p>{scanResultWebCam}</p>
+    <p>{scanResultWebCam}</p> */}
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{data}</p>
   </div>
   );
 }
